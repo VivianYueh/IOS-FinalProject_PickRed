4b60d703ec9a49537adff8327c02e094684d8854
VivianYueh <01057113@email.ntou.edu.tw>
2023-05-05 15:50:24 +0800

    Initial Commit

---
+++ Final_project/Final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_projectApp.swift
+//  Final_project
+//
+//  Created by 岳紀伶 on 2023/5/5.
+//
+
+import SwiftUI
+
+@main
+struct Final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
